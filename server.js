91b8d78b740bef2dcce6b4ef00f755c1325e0d7d
Dolores Cuadra <lola_cuadra1940@byui.edu>
2024-04-10 17:27:20 -0600

    Updates to server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080; 
 const app = express();
 const { auth, requiresAuth } = require('express-openid-connect');
 const swaggerUi = require('swagger-ui-express');
@@ -19,25 +19,23 @@ const config = {
   issuerBaseURL: process.env.ISSUEBASERURL
 };
 
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
-
-// Apply authentication middleware globally
-app.use(auth(config));
-
-// Serve static files (Swagger UI assets)
-app.use(express.static('public'));
-
+// Middleware
 app.use(bodyParser.json());
+app.use(express.static('public'));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
 
 // Routes setup
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.use(auth(config));
 app.use('/', require('./routes'));
 
+// Database connection
 const db = require('./models');
 db.mongoose
   .connect(db.url, {
@@ -45,24 +43,35 @@ db.mongoose
     useUnifiedTopology: true
   })
   .then(() => {
+    // Start the server
     app.listen(port, () => {
       console.log(`DB Connected and server running on ${port}.`);
     });
   })
   .catch((err) => {
-    console.log('Cannot connect to the database!', err);
-    process.exit();
+    console.error('Cannot connect to the database:', err);
+    process.exit(1); // Exit the process with an error code
   });
 
+// Login route
 app.get('/login', (req, res) => {
   if (req.query && req.query.code) {
-    // Authentication successful, redirect the user to the desired destination
     res.redirect('https://task-cse341-final.onrender.com/api-docs');
   } else {
-    // Authentication failed or no code received
     res.status(500).send('Authentication failed');
   }
 });
 
 // Swagger UI setup
 app.use('/api-docs', requiresAuth(), swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error('An error occurred:', err);
+  res.status(500).send('Something went wrong');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
